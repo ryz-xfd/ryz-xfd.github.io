0ff6ea6a81a4bec210ef83893a15d1a085f6f85b
ryz <xcvxddvbghjng@gmail.com>
2024-07-13 15:40:12 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('myAudio');
+    audio.play(); // Tự động phát nhạc khi trang web được tải lên
+});
